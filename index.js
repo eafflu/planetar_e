06064196dcea2b69be9cc8e510cedb92a74f8c4c
Edem Afflu <edemakuaafflu@gmail.com>
2019-01-30 02:53:22 +0000

    * add getDistance method to phyz

--- index.js
+++ index.js
@@ -3,17 +3,19 @@
   window.planetar_e = window.planetar_e || {
     
     numz:{
+      
+          
+          
+      
+    },
+    phyz:{
       getDistance(pointA, pointB){
         const
           distanceX = pointB.x - pointA.x,
           distanceY = pointB.y - pointA.y,
           distance = Math.sqrt(distanceX * distanceX + distanceY * distanceY)
-        
-          
-          
       }
     },
-    phyz:{},
       
   };
   
